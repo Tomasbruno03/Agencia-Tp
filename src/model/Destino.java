c3777b5ae489514c22649baad5cdd57ad24f09a9
Tomas <tomasbruno5451@gmail.com>
2025-10-29 14:52:25 -0300

    Cree la clase destino

--- src/model/Destino.java
+++ src/model/Destino.java
@@ -1,4 +1,27 @@
 package model;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Destino {
+    private String Nombre;
+    private float CantKm;
+
+    public Destino(String Nom, float Ckm){
+        Nombre=Nom;
+        CantKm=Ckm;
+    }
+    public Destino(){}
+
+    public String getNombre() {
+        return Nombre;
+    }
+    public float getCantKm() {
+        return CantKm;
+    }
+
+    @Override
+    public String toString() {
+        return "Destino{nombre='" + Nombre + "', cantKm=" + CantKm + "}";
+    }
+
 }
