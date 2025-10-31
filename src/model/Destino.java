c823939822fce829a21cd2ee0fb8545d52fc56ed
Tomas <tomasbruno5451@gmail.com>
2025-10-30 21:32:37 -0300

    Arregle una boludez

--- src/model/Destino.java
+++ src/model/Destino.java
@@ -1,8 +1,9 @@
 package model;
 import java.util.Locale;
 import java.util.Scanner;
+import java.utils.Objects;
 
-public class Destino {
+public class Destino implements Comparable <Destino>{
     private String Nombre;
     private float CantKm;
 
@@ -24,4 +25,19 @@ public class Destino {
         return "Destino{nombre='" + Nombre + "', cantKm=" + CantKm + "}";
     }
 
+    @Override
+    public int compareTo(Destino otro)
+    {
+        return Nombre.compareTo(otro.Nombre);
+    }
+
+    @Override
+    public Boolean equals(Object o){
+        if(this == o) //son el mismo objeto en memoria?
+            return true;
+        if(o == null || getClass() != o.getClass()) // o el objeto es nulo o son de clases distintas
+            return false;
+        ResponsableABordo that  = (ResponsableABordo) o; //casteo necesario para usar una variable de tipo clase
+        return Objects.equals(this.Nombre,that.Nombre);
+    }
 }
