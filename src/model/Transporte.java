9b1d04bab53bde07d02aea45b7b461ea339165c8
Tatumerlino <tatumerlino@gmail.com>
2025-11-03 11:25:56 -0300

    Corregí metodos getters y setters

--- src/model/Transporte.java
+++ src/model/Transporte.java
@@ -3,10 +3,10 @@ package model;
 import java.util.*;
 import java.util.Objects;
 
-public abstract class Transporte { //Constructor, getters y settters
+public abstract class Transporte {
     private String patente;
     private int capacidadPasajeros;
-    private Set<Viaje> listaViajes;
+    private Set<Viaje> listaViajes; // atributo, Constructor, getters, settters, toCompare, toString
     private boolean disponible;
     private float velocidadPromedioXhora;
 
@@ -18,15 +18,20 @@ public abstract class Transporte { //Constructor, getters y settters
         this.velocidadPromedioXhora = velocidadPromedioXhora;
     }
 
-    public abstract String getPatente();
-    public abstract int getCapacidadPasajeros(); // es igual a capacidad maxima
-    //
-    public abstract boolean estaDisponible();
-    public abstract float getVelocidadPromedioXhora();
+    public String getPatente(){ return patente; }
+    public int getCapacidadPasajeros(){ return capacidadPasajeros; }
+    public boolean estaDisponible(){ return disponible; }
+    public float getVelocidadPromedioXhora(){ return velocidadPromedioXhora; }
 
-    public abstract boolean setDisponible(boolean disponible); // DUDA1
-    public abstract float setVelocidadPromedioXhora(float velocidadPromedioXhora); // DUDA2
-    //
+    public boolean setDisponible(boolean disponible){
+        if (disponible != this.disponible)
+            this.disponible = disponible;
+    }
+
+    public float setVelocidadPromedioXhora(float velocidadPromedioXhora){
+        if(velocidadPromedioXhora > 0)
+            this.velocidadPromedioXhora = velocidadPromedioXhora;
+    }
 
     public String toString() {
         return "Transporte [ Patente: " + patente + ", Capacidad de Pasajeros: " + capacidadPasajeros + ", Disponible: " + disponible + ", Velocidad promedio por hora: " + velocidadPromedioXhora" ]";
