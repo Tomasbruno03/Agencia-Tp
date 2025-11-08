9aa7c49c059d2e03db883a7cc29eebaf9584e443
Tomas <tomasbruno5451@gmail.com>
2025-11-08 20:53:08 -0300

    Termine la clase viaje

--- src/model/Viaje.java
+++ src/model/Viaje.java
@@ -14,7 +14,7 @@ public abstract class Viaje implements Comparable <Viaje>{
     private int idViaje; //Es clave primariaAutoincremental
     private String nombre; // El nombre es unico, del viaje del Destino y el Id
     private Destino destinoDelViaje;
-    private int cantPasajeros; // Empieza en 0
+    private int cantPasajeros; // Empieza en 1. Suponiendo que al crear el viaje es porque al menos un pasajero lo solicito
     private estado estadoActual;
     private Set<ResponsableABordo>Responsables= new HashSet<>();
     private float avanceKmRecorridos;
@@ -22,13 +22,15 @@ public abstract class Viaje implements Comparable <Viaje>{
 
 
     // CONSTRUCTOR
-    public Viaje(int idVia, String nom, Destino destinoViaje) {
+    public Viaje(int idVia, String nom, Destino destinoViaje,Transporte t) {
         this.idViaje = idVia;
         this.nombre = nom;
         this.destinoDelViaje = destinoViaje;
         this.cantPasajeros = 1;
         this.estadoActual = estado.PENDIENTE; // Estado inicial
         this.avanceKmRecorridos = 0;
+        this.TransporteAsignado=t;
+
     }
     /*
     GETTERS
@@ -56,6 +58,10 @@ public abstract class Viaje implements Comparable <Viaje>{
         return avanceKmRecorridos;
     }
 
+    public Transporte getTransporteAsignado() {
+        return TransporteAsignado;
+    }
+
     @Override
     public int compareTo(Viaje o) {
         return nombre.compareTo(o.getNombre());
@@ -70,8 +76,9 @@ public abstract class Viaje implements Comparable <Viaje>{
 
     @Override
     public int hashCode() {
-        return Objects.hash(idViaje, nombre, estadoActual);
+        return Objects.hash(idViaje);
     }
+
     public void iniciar() {
         if (estadoActual != estado.PENDIENTE)
             throw new IllegalStateException("El viaje ya fue iniciado o finalizado.");
@@ -92,8 +99,67 @@ public abstract class Viaje implements Comparable <Viaje>{
         if (estadoActual != estado.EN_CURSO)
             throw new IllegalStateException("Solo se puede finalizar un viaje en curso.");
         estadoActual = estado.FINALIZADO;
-        //liberarResponsables(); #### Aun no esta definida
+        liberarResponsables();
     }
+    public estado getEstado(){return estadoActual;};
     public abstract float calcularCosto();
+
+    public void liberarResponsables()
+    {
+        Responsables.clear();
+    }
+
+    public void AgregarResponsable(ResponsableABordo r)
+    {
+        this.Responsables.add(r);
+    }
+
+    public void QuitarResponsable(ResponsableABordo r)
+    {
+        this.Responsables.remove(r);
+    }
+
+    public void AgregarPasajeros()
+    {
+        if(TransporteAsignado!=null&&(cantPasajeros==TransporteAsignado.getCapacidadPasajeros())){
+            throw new IllegalStateException("No hay capacidad disponible en el transporte.");
+        }else{
+            this.cantPasajeros++;
+        }
+
+    }
+
+    public void QuitarPasajeros()
+    {
+        this.cantPasajeros--;
+    }
+    public void setTransporteAsignado(Transporte transporte) {
+        this.TransporteAsignado = transporte;
+    }
+    public boolean estaPendiente() {
+        return estadoActual == estado.PENDIENTE;
+    }
+
+    public boolean estaEnCurso() {
+        return estadoActual == estado.EN_CURSO;
+    }
+
+    public boolean estaFinalizado() {
+        return estadoActual == estado.FINALIZADO;
+    }
+    public float getKmTotales() {
+        return destinoDelViaje.getCantKm();
+    }
+
+    public float getKmRestantes() {
+        return getKmTotales() - avanceKmRecorridos;
+    }
+
+    public float getPorcentajeAvance() {
+        float total = getKmTotales();
+        return total <= 0 ? 0 : (avanceKmRecorridos / total) * 100f;
+    }
+
+
 
 }
