2a6907239b361d87a8bc41381d80118408e6e510
Sofia <sofichalop123@gmail.com>
2025-11-11 23:43:29 -0300

    Guarde mis cambios

---
+++ src/model/CortaDistancia.java
@@ -0,0 +1,23 @@
+package model;
+
+public class CortaDistancia extends Viaje{
+    public float cobrobase;
+
+    public CortaDistancia(int idVia, String nom, Destino destinoViaje,float cobrobase){
+        super(idVia,nom,destinoViaje);
+        this.cobrobase=cobrobase;
+    }
+
+    public float getCobrobase(){
+        return cobrobase;
+    }
+
+    public void setCobrobase(float cobrobase){
+        this.cobrobase=cobrobase;
+    }
+
+    @Override
+    public String toString(){
+        return "CortaDistancia [Cobro base en viajes de corta distancia:" + cobrobase + " ]";
+    }
+}
