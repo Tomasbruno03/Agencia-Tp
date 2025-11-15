2a03728109622679a4ed7aa06cf4858ac45cf619
Tomas <tomasbruno5451@gmail.com>
2025-11-15 15:11:08 -0300

    Hice mas robusto a errores la clase Destino

--- src/controller/TransporteController.java
+++ src/controller/TransporteController.java
@@ -6,22 +6,7 @@ import java.util.*;
 public class TransporteController {
     private Agencia agencia = Agencia.getInstance(); //ver
 
-    public void asociarTransporteADestino(String patente, String nombreDestino)
-        throws Exception{
-        Transporte transporte = agencia.buscarTransportePorPatente(patente);
-            if(transporte == null)
-                throw new Exception("No existe transporte con patente: " + patente);
 
-
-        Destino destino = agencia.buscarDestinoPorNombre(nombreDestino);
-        if(destino == null)
-            throw new Exception("No existe destino con nombre: " + nombreDestino);
-
-        agencia.asociarTransporteADestino(destino, transporte);
-
-        System.out.println("Transporte " + patente + " asociado correctamente a destino " + nombreDestino);
-
-    }
 
     public Set<Transporte> obtenerTransportePorDestino(String nombreDestino) throws Exception{
         Destino destino = agencia.buscarDestinoPorNombre(nombreDestino);
